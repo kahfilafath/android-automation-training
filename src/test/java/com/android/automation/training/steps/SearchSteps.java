a21e94665f7b7a0fcecfd161bfde226661a77ec3
MursidSetyo <setyomursid@gmail.com>
2023-06-05 12:47:17 +0700

    3nd commit

--- src/test/java/com/android/automation/training/steps/SearchSteps.java
+++ src/test/java/com/android/automation/training/steps/SearchSteps.java
@@ -25,18 +25,18 @@ public class SearchSteps extends PageBaseObject {
     }
 
 
-    @And("user search the product “ultra milk susu uht coklat” on the search bar")
-    public void userSearchTheProductUltraMilkSusuUhtCoklatOnTheSearchBar() {
-        By INPUT_KEYWORD = MobileBy.id("edt_search");
-        input(INPUT_KEYWORD,"ultra milk susu uht coklat");
-    }
+//    @And("user search the product “ultra milk susu uht coklat” on the search bar")
+//    public void userSearchTheProductUltraMilkSusuUhtCoklatOnTheSearchBar() {
+//        By INPUT_KEYWORD = MobileBy.id("edt_search");
+//        input(INPUT_KEYWORD,"ultra milk susu uht coklat");
+//    }
 
-    @And("user see the product suggestion “ultra milk susu uht coklat” below the search bar")
-    public void userSeeTheProductSuggestionUltraMilkSusuUhtCoklatBelowTheSearchBar() {
-        By SUGGESTION = MobileBy.id("tv_keyword");
-        Assertions.assertEquals("ultra milk susu uht coklat",getText(SUGGESTION),"Failed load suggestion");
-
-    }
+//    @And("user see the product suggestion “ultra milk susu uht coklat” below the search bar")
+//    public void userSeeTheProductSuggestionUltraMilkSusuUhtCoklatBelowTheSearchBar(String title_sugest) {
+//        By SUGGESTION = MobileBy.id("tv_keyword");
+//        Assertions.assertEquals(title_sugest,getText(SUGGESTION),"Failed load suggestion");
+//
+//    }
 
     @And("user click the product suggestion below the search bar")
     public void userClickTheProductSuggestionBelowTheSearchBar() {
@@ -45,10 +45,28 @@ public class SearchSteps extends PageBaseObject {
 
     }
 
-    @Then("user should discover the product “ultra milk susu uht coklat” on the search result page")
-    public void userShouldDiscoverTheProductUltraMilkSusuUhtCoklatOnTheSearchResultPage() {
-        By RESULT = MobileBy.id("txt_product_name");
-        Assertions.assertEquals("ULTRA MILK Susu UHT Coklat 1 L",getText(RESULT),"Failed load product");
+//    @Then("user should discover the product “ULTRA MILK Susu UHT Coklat 1 L” on the search result page")
+//    public void userShouldDiscoverTheProductUltraMilkSusuUhtCoklatOnTheSearchResultPage(String title_discover) {
+//        By RESULT = MobileBy.id("txt_product_name");
+//        Assertions.assertEquals(title_discover,getText(RESULT),"Failed load product");
+//
+//    }
 
+    @And("user search the product {string} on the search bar")
+    public void userSearchTheProductOnTheSearchBar(String input_search) {
+        By INPUT_KEYWORD = MobileBy.id("edt_search");
+        input(INPUT_KEYWORD,input_search);
+    }
+
+    @And("user see the product suggestion {string} below the search bar")
+    public void userSeeTheProductSuggestionBelowTheSearchBar(String see_product) {
+        By SUGGESTION = MobileBy.id("tv_keyword");
+        Assertions.assertEquals(see_product,getText(SUGGESTION),"Failed load suggestion");
+    }
+
+    @Then("user should discover the product {string} on the search result page")
+    public void userShouldDiscoverTheProductOnTheSearchResultPage(String title_product) {
+        By RESULT = MobileBy.id("txt_product_name");
+        Assertions.assertEquals(title_product,getText(RESULT),"Failed load product");
     }
 }
