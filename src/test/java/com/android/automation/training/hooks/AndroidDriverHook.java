e68a4922a3e49a296e6f7e995279b4a23f11fcf4
kahfi <kahfialfath@gmail.com>
2024-04-22 10:47:54 +0700

    update hooks to add sc

--- src/test/java/com/android/automation/training/hooks/AndroidDriverHook.java
+++ src/test/java/com/android/automation/training/hooks/AndroidDriverHook.java
@@ -1,7 +1,14 @@
 package com.android.automation.training.hooks;
 
+import static com.android.automation.training.drivers.AndroidDriverInit.driver;
+
 import com.android.automation.training.drivers.AndroidDriverInit;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import java.io.File;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class AndroidDriverHook {
 
@@ -9,6 +16,16 @@ public class AndroidDriverHook {
   @Before
   public void init(){
     init.initialize();
+  }
+  @After()
+  public void afterScenario(Scenario scenario){
+    if(scenario.isFailed()){
+      TakesScreenshot ts = driver;
+      byte[] src = ts.getScreenshotAs(OutputType.BYTES);
+      File srcFile = ts.getScreenshotAs(OutputType.FILE);
+      scenario.attach(src, "image/png", "screenshot");
+      File file = new File(srcFile.toURI());
+    }
   }
 
 }
